649c2909330ad0cd10d4547d9f99d779c0d3a0b0
Isinaveira <isinaveira@gmail.com>
2024-08-12 15:00:01 -0400

    First Commit with auth method

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,25 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const authHeader = req.headers.authorization;
+    //Comprobamos si existe la cabecera 
+    if (!authHeader) {
+        return res.status(401).json({ message: "No se ha pasado un token" });
+    }
+    //Obtenemos el token 
+    const token = authHeader.split(' ')[1]; 
+
+    //Verificamos la integridad del token
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Token incorrecto" });
+        }
+
+        //Si el token es correcto devolvemos el decodificado
+        req.user = decoded;
+        next();
+    });
+
 });
  
 const PORT =5000;
